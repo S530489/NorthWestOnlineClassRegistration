93116b844ce707a609ae79400ea59812d149cdc1
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-12-03 01:02:27 -0600

    add ejs files and wiring between them using nodejs

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+console.log('Inside routes/index')
+
+
+router.get("/userSignup", function (req, res) {
+  res.render("userSignup.ejs");
+});
+
+router.get("/userLogin", function (req, res) {
+  res.render("userLogin.ejs");
+});
+
+router.post("/userHome", function (req, res) {
+  res.render("userHome.ejs");
+});
+
+router.get("/userSearch", function (req, res) {
+  res.render("userSearch.ejs");
+});
+
+router.get("/userRegisterDrop", function (req, res) {
+  res.render("userRegisterDrop.ejs");
+});
+
+//Admin routes
+
+router.get("/adminSignup", function (req, res) {
+  res.render("adminSignup.ejs");
+});
+
+router.get("/adminLogin", function (req, res) {
+  res.render("adminLogin.ejs");
+});
+
+router.post("/adminHome", function (req, res) {
+  res.render("adminHome.ejs");
+});
+
+router.get("/adminSearch", function (req, res) {
+  res.render("adminSearch.ejs");
+});
+
+
+router.get("/addCourse", function (req, res) {
+  res.render("adminAddCourse.ejs");
+});
+
+router.get("/updateDeleteCourse", function (req, res) {
+  res.render("adminUpdateDelete.ejs");
+});
+
+
+
+
+
+module.exports = router
+
